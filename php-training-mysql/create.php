6cd56150a96fd934c82e8b7f5f77a9bf323d156b
Christophe Schevers <christophe.schevers@gmail.com>
2019-04-12 16:57:33 +0200

    New commit

--- php-training-mysql/create.php
+++ php-training-mysql/create.php
@@ -1,13 +1,44 @@
+<?php
+// Connect from connect.php file
+include('connect.php');
+
+// Insert data
+if (isset($_POST['button']) & !empty($_POST))
+{
+	$name = $_POST['name'];
+	$difficulty = $_POST['difficulty'];
+	$distance = $_POST['distance'];
+	$duration = $_POST['duration'];
+	$heightdiff = $_POST['height_difference'];
+
+	$data = [
+		':name' => $name,
+		':difficulty' => $difficulty,
+		':distance' => $distance,
+		':duration' => $duration,
+		':height_difference' => $heightdiff
+	];
+
+	$sqlInsert = "INSERT INTO `hiking` (`name`, `difficulty`, `distance`, `duration`, `height_difference`) VALUES ($name, $difficulty, $distance, $duration, $heightdiff)";
+
+	$sqlExec = $pdo->prepare($sqlInsert);
+	$sqlExec->execute($data);
+
+	header("Location: create.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Ajouter une randonnée</title>
+	<title>Add a Hike</title>
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
-	<h1>Ajouter</h1>
+	<a href="read.php">Datalist</a>
+	<h1>Add</h1>
 	<form action="" method="post">
 		<div>
 			<label for="name">Name</label>
@@ -15,13 +46,13 @@
 		</div>
 
 		<div>
-			<label for="difficulty">Difficulté</label>
+			<label for="difficulty">Difficulty</label>
 			<select name="difficulty">
-				<option value="très facile">Très facile</option>
-				<option value="facile">Facile</option>
-				<option value="moyen">Moyen</option>
-				<option value="difficile">Difficile</option>
-				<option value="très difficile">Très difficile</option>
+				<option value="very easy">Very easy</option>
+				<option value="easy">Easy</option>
+				<option value="medium">Medium</option>
+				<option value="hard">Hard</option>
+				<option value="very hard">Very hard</option>
 			</select>
 		</div>
 
@@ -30,14 +61,14 @@
 			<input type="text" name="distance" value="">
 		</div>
 		<div>
-			<label for="duration">Durée</label>
-			<input type="time" name="duration" value="">
+			<label for="duration">Duration</label>
+			<input type="text" name="duration" value="">
 		</div>
 		<div>
-			<label for="height_difference">Dénivelé</label>
+			<label for="height_difference">Height difference</label>
 			<input type="text" name="height_difference" value="">
 		</div>
-		<button type="submit" name="button">Envoyer</button>
+		<button type="submit" name="button">Send</button>
 	</form>
 </body>
 </html>
--- php-training-mysql/read.php
+++ php-training-mysql/read.php
@@ -1,19 +1,5 @@
 <?php
-// User data variables
-$servername = 'localhost';
-$username = 'root';
-$password = 'root';
-$db = 'reunion_island';
-
-// Connect
-try 
-{
-  $pdo = new PDO("mysql:host=$servername; dbname=$db; charset=utf8mb4",$username, $password);
-}
-catch(PDOException $e)
-{
-  die ("Connection failed: ". $e->getMessage());
-}
+include('connect.php');
 
 // Get data
 $sql = "SELECT * FROM hiking";
