eb1ba3c321862886212715236c89a547343c7d4f
Christophe Schevers <christophe.schevers@gmail.com>
2019-04-23 16:58:44 +0200

    New commit

--- php-exercises-crud2/exercise-1.php
+++ php-exercises-crud2/exercise-1.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +12,28 @@
     <title>Colyseum Forms</title>
 </head>
 <body>
-    
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="firstName">First Name</label>
+                <input type="text" name="firstName">
+                <label for="lastName">Last Name</label>
+                <input type="text" name="lastName">
+            </div>
+            <div class="form-group">
+                <label for="birthDate">Date of Birth</label>
+                <input type="date" name="birthDate">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="checkBox">
+                <label class="form-check-label" for="checkBox">Loyalty Card</label>
+            </div>
+            <div class="form-group">
+                <label for="cardNumber">Loyalty Card Number</label>
+                <input type="number" name="cardNumber" disabled>
+            </div>
+        </form>
+    </div>
 
     <!-- Bootstrap scripts -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
